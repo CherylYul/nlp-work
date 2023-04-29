8879223f1f3440cc9c33225fa8a69ead5ce62b69
ycheryl <yulcheryl2357@gmail.com>
2023-04-29 13:01:59 +0700

    update file

--- classify-digits.py
+++ classify-digits.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import mnist_loader
 
 
 class Network(object):
@@ -27,9 +28,9 @@ class Network(object):
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print(f"Epoch {0}: {1}/{2}".format(j, self.evaluate(test_data), n_test))
+                print("Epoch {0}: {1}/{2}".format(j, self.evaluate(test_data), n_test))
             else:
-                print(f"Epoch {0} complete".format(j))
+                print("Epoch {0} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -45,9 +46,44 @@ class Network(object):
             b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)
         ]
 
-    def backprop():
-        pass
+    def backprop(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        activation = x
+        activations = [x]
+        zs = []
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, test_data):
+        test_result = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_result)
+
+    def cost_derivative(self, output_activations, y):
+        return output_activations - y
 
 
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
